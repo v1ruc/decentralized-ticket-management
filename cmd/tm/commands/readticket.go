4a39fc524e2463c9c201b0f4d6d48b958cfa0ebc
Dmitrij Koniajev <dimchansky@gmail.com>
2019-08-24 19:41:57 +0200

    added arguments for read-ticket command

--- cmd/tm/commands/readticket.go
+++ cmd/tm/commands/readticket.go
@@ -1,7 +1,20 @@
 package commands
 
+import (
+	"github.com/ethereum/go-ethereum/log"
+	"github.com/monetha/go-verifiable-data/cmd/privatedata-exchange/commands/flag"
+)
+
 // ReadTicketCommand handles `read-ticket` command
-type ReadTicketCommand struct{}
+type ReadTicketCommand struct {
+	EventDIDAddress       flag.EthereumAddress         `long:"eventaddr"        required:"true" description:"Ethereum address of event DID contract"`
+	ParticipantDIDAddress flag.EthereumAddress         `long:"myaddr"           required:"true" description:"Ethereum address of participant DID contract"`
+	PrivateKey            flag.ECDSAPrivateKeyFromFile `long:"privatekey"       required:"true" description:"private key filename of participant"`
+	BackendURL            string                       `long:"backendurl"       required:"true" description:"Ethereum backend URL"    default:"https://ropsten.infura.io"`
+	IPFSURL               string                       `long:"ipfsurl"                          description:"IPFS node URL"           default:"https://ipfs.infura.io:5001"`
+	Verbosity             log.Lvl                      `long:"verbosity"                        description:"log verbosity (0-9)"     default:"2"`
+	VModule               string                       `long:"vmodule"                          description:"log verbosity pattern"`
+}
 
 // Execute implements flags.Commander interface
 func (c *ReadTicketCommand) Execute(args []string) error {
